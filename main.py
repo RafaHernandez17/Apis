cca556506594900e938d61b94f63ded1afdee2ca
Rafael <brayan.h1705@gmail.com>
2022-09-13 23:42:35 -0500

    Usando el metodo Args

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import requests
 
 if __name__ == '__main__':
-    url = 'https://www.google.com.mx/'
-    response = requests.get(url)
+    url = 'http://httpbin.org/get'
+    args = {'nombre': 'Rafael', 'curso': 'python'}
 
-    #Muestra la estructura html del url
-    #if response.status_code == 200:
-        #print(response.content)
-    
-    #Guardar el contenido de la pag web
+    response = requests.get(url, params=args)
+    print(response.url)
 
     if response.status_code == 200:
         content = response.content
-        file = open('google.html', 'wb')
-        file.write(content)
-        file.close()
+        print(content)
+    
