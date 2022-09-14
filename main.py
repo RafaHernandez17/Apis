af4edcefa4e7d9229b5f46557e70a71b1bc1bdf0
Rafael <brayan.h1705@gmail.com>
2022-09-13 23:57:33 -0500

    Metodo POST

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import requests
 import json
 
 if __name__ == '__main__':
-    url = 'http://httpbin.org/get'
-    args = {'nombre': 'Rafael', 'curso': 'python'}
+    url = 'http://httpbin.org/post'
+    payload = {'nombre': 'Rafael', 'curso': 'python'}
 
-    response = requests.get(url, params=args)
+    #response = requests.post(url, json=payload)
+    response = requests.post(url, data=json.dumps(payload))
+
+    #json post se encarga de serializarlos
+    #data entonces nosotros de serializarlos
     print(response.url)
 
     if response.status_code == 200:
-        """
-        response_json = response.json() #Dic
-        origin = response_json['origin']
-        print(origin)
-        """
-
-        response_json = json.loads(response.text)
-        origin = response_json['origin']
-        print(origin)
+        print(response.content)
