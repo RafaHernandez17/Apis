35087ecca12cad9cfd00989d5db7528e34a5b660
Rafael <brayan.h1705@gmail.com>
2022-10-03 20:35:39 -0500

    Sesiones

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+from urllib import response
 import requests
 
 if __name__ == '__main__':
     
-    url = 'https://httpbin.org/cookies'
-    cookies = {'my-cookie': 'Trues'}
+    url = 'https://api.github.com/user'
 
-    response = requests.get(url, cookies=cookies)
+    session = requests.session()
+    session.auth = ('al063778@uacam.mx','5289hudson')
 
+    response = session.get(url)
     if response.ok:
-        cookies = response.cookies
-        print(cookies)
-
-        print("El contenido es:")
-        print(response.content)
-    
+        response = session.get('https://github.com/RafaHernandez17')
+        if response.ok:
+            print(response.cookies)
